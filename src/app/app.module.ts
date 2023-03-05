40239dfe72ca0be5b2c0d0bce0212f70fd6eb128
fjmoeller <flomodoyt@gmail.com>
2023-03-05 21:46:57 +0100

    added angular universal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,10 @@ import { AboutComponent } from './components/about/about.component';
 import { FormsModule } from '@angular/forms';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from '../environments/environment';
 import { NgbModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { MocGrabberService } from './services/moc-grabber.service';
-import { ScullyLibModule } from '@scullyio/ng-lib';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -37,11 +35,9 @@ import { ScullyLibModule } from '@scullyio/ng-lib';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
     NgbTooltipModule,
     NgbModule,
-    ScullyLibModule
+    RouterModule
   ],
   providers: [MocGrabberService],
   bootstrap: [AppComponent]
--- src/app/services/moc-grabber.service.ts
+++ src/app/services/moc-grabber.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { filter, map, Observable } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { Moc } from '../components/classes';
+import mocs from '../../assets/mocs.json';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MocGrabberService {
 
-  private mocs! : Observable<Moc[]>;
+  private mocs!: Observable<Moc[]>;
 
-  constructor(private firestore: AngularFirestore) {this.mocs = this.firestore.collection<Moc>('mocs').valueChanges(); }
+  constructor() {
+    let parsedMocData: Moc[] = mocs;
+    console.log(parsedMocData[0])
+    this.mocs = of(parsedMocData).pipe(tap(moc => console.log("Mocs received:", moc.length)));
+  }
 
 
   getMoc(id: number): Observable<Moc> {

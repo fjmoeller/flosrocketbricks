e9bd959042c22317c6e846707f76d7714926c308
fjmoeller <flomodoyt@gmail.com>
2023-03-23 15:10:03 +0100

    Revert "cleanup"
    This reverts commit bbe38ba7ac45944d850d9c6995f9ebbd2dd2234d.

--- src/app/services/moc-grabber.service.ts
+++ src/app/services/moc-grabber.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+//import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { map, Observable, tap } from 'rxjs';
 import { Moc } from '../components/classes';
 import mocs from '../../assets/mocs.json';
@@ -11,8 +12,9 @@ export class MocGrabberService {
 
   private mocs! : Observable<Moc[]>;
 
-  constructor() {
-
+  constructor(/*private firestore: AngularFirestore*/) {
+    /*this.mocs = this.firestore.collection<Moc>('mocs').valueChanges(); */
+    
     let parsedMocData : Moc[] = mocs;
     console.log(parsedMocData[0])
     this.mocs = of(parsedMocData).pipe(tap(moc => console.log("Mocs received:",moc.length)));

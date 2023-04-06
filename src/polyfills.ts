d4f59557e43588e888a59113405056e60992d577
fjmoeller <flomodoyt@gmail.com>
2023-04-06 17:34:53 +0200

    Update polyfills.ts

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -55,10 +55,5 @@ import 'zone.js';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-
-/***************************************************************************************************
-* SCULLY IMPORTS
-*/
-// tslint:disable-next-line: align
 import 'zone.js/dist/task-tracking';
-import "web-streams-polyfill/ponyfill";
+import "web-streams-polyfill";

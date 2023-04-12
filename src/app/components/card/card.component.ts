85283b44740a1a0a53e3f1aced3ecb252f5ac8a4
fjmoeller <flomodoyt@gmail.com>
2023-04-12 12:12:52 +0200

    fixed scale not showing correctly on the startpage

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -9,7 +9,7 @@ export class CardComponent implements OnInit {
 
   @Input('pic') coverPicture = "https://pbs.twimg.com/profile_images/1557001528912842757/XujDja68_400x400.jpg"
   @Input('name') mocName = "Test MOC"
-  @Input('scale') scale = "1:110"
+  @Input('scale') scale = "110"
   @Input('parts') parts: number = 101
   @Input('id') id: number = 0;
 

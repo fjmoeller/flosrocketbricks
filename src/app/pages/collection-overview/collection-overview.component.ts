cd3202a234cb2d232639647ff8592ed61f784c75
fjmoeller <flomodoyt@gmail.com>
2023-05-28 16:26:00 +0200

    fix missing metadata

--- src/app/pages/collection-overview/collection-overview.component.ts
+++ src/app/pages/collection-overview/collection-overview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Collection } from 'src/app/model/classes';
 import { CollectionGrabberService } from 'src/app/services/collection-grabber.service';
+import { MetaServiceService } from 'src/app/services/meta-service.service';
 
 @Component({
   selector: 'app-collection-overview',
@@ -11,10 +12,11 @@ export class CollectionOverviewComponent implements OnInit {
 
   collections: Collection[] = [];
 
-  constructor(private collectionGrabberService: CollectionGrabberService) { }
+  constructor(private collectionGrabberService: CollectionGrabberService,private metaService: MetaServiceService) { }
 
   ngOnInit(): void {
     this.collections = this.collectionGrabberService.getAllCollections().sort((a: Collection, b: Collection) => b.name < a.name ? 1 : -1);
+    this.metaService.setDefaultTags("Collections - FlosRocketBricks","https://flosrocketbricks.com/collections/");
   }
 
 }

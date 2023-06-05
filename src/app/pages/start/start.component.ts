ff343c1fa201f5b0517102ded05d6080244a968e
fjmoeller <flomodoyt@gmail.com>
2023-06-05 12:53:02 +0200

    stuff

--- src/app/pages/start/start.component.ts
+++ src/app/pages/start/start.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { MocGrabberService } from 'src/app/services/moc-grabber.service';
-import { Moc } from '../../model/classes';
+import { Collection, Moc } from '../../model/classes';
 import { MetaServiceService } from 'src/app/services/meta-service.service';
+import { CollectionGrabberService } from 'src/app/services/collection-grabber.service';
 
 @Component({
   selector: 'app-start',
@@ -13,14 +14,16 @@ import { MetaServiceService } from 'src/app/services/meta-service.service';
 export class StartComponent implements OnInit {
 
   newestMocs: Moc[] = [];
+  collections: Collection[] = [];
 
-  constructor(private metaService: MetaServiceService, private mocGrabberService: MocGrabberService) {
+  constructor(private metaService: MetaServiceService, private mocGrabberService: MocGrabberService,private collectionGrabberService: CollectionGrabberService) {
   }
 
   ngOnInit(): void {
     this.metaService.setDefaultTags("Start - FlosRocketBricks", "https://flosrocketbricks.com/");
 
     this.newestMocs = this.mocGrabberService.getAllMocs().sort((a: Moc, b: Moc) => b.id - a.id).slice(0, 6);
+    this.collections = this.collectionGrabberService.getAllCollections().sort((a: Collection, b: Collection) => b.id - a.id).slice(0, 6);
   }
 
 }
